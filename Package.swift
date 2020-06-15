8d8b8666b4fda2d7dbd4cd010f5f143f851cbba3
Omar Alshammari <o.alshammari@Mac-mini.local>
2020-06-16 02:34:26 +0300

    upgrade to vapor 4

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "wallet-service",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(name: "wallet-service", targets: ["RestApi"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+//        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
         
         // Lightweight full-featured Promises, Async & Await Library in Swift.
@@ -20,15 +23,35 @@ let package = Package(
     targets: [
         .target(name: "Domain"),
         .target(name: "Utilties"),
-        .target(name: "Application", dependencies: ["Domain", "Utilties", "Hydra"]),
-        .target(name: "Persistance", dependencies: ["Domain", "Utilties", "Application", "Hydra"]),
-        .target(name: "RestApi", dependencies: ["FluentSQLite", "Vapor", "Application", "Utilties", "Persistance"]),
-        .target(name: "Run", dependencies: ["RestApi"]),
-        .testTarget(name: "DomainTests", dependencies: ["Domain"]),
-        .testTarget(name: "UtiltiesTests", dependencies: ["Utilties"]),
-        .testTarget(name: "ApplicationTests", dependencies: ["Application", "Persistance"]),
-        .testTarget(name: "PersistanceTests", dependencies: ["Persistance"]),
-        .testTarget(name: "RestApiTests", dependencies: ["RestApi"])
+        .target(name: "Application",
+                dependencies: [.target(name: "Domain"),
+                               .target(name: "Utilties"),
+                               .product(name: "Hydra", package: "Hydra")]),
+        .target(name: "Persistance",
+                dependencies: [.target(name: "Domain"),
+                               .target(name: "Utilties"),
+                               .target(name: "Application"),
+                               .product(name: "Hydra", package: "Hydra")]),
+        .target(name: "RestApi",
+                dependencies: [.product(name: "Vapor", package: "vapor"),
+                               .target(name: "Application"),
+                               .target(name: "Utilties"),
+                               .target(name: "Persistance")]),
+        .target(name: "Run",
+                dependencies: [.target(name: "RestApi")]),
+        
+        // Tests
+        .testTarget(name: "DomainTests",
+                    dependencies: [.target(name: "Domain")]),
+        .testTarget(name: "UtiltiesTests",
+                    dependencies: [.target(name: "Utilties")]),
+        .testTarget(name: "ApplicationTests",
+                    dependencies: [.target(name: "Application"),
+                                   .target(name: "Persistance")]),
+        .testTarget(name: "PersistanceTests",
+                    dependencies: [.target(name: "Persistance")]),
+        .testTarget(name: "RestApiTests",
+                    dependencies: [.target(name: "RestApi")])
     ]
 )
 
