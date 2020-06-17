00ebe60c856a9c2bdca31479f95f4cebf94a8ae6
Omar Alshammari <o.alshammari@Mac-mini.local>
2020-06-17 18:19:11 +0300

    add repo test skeleton

---
+++ Tests/RepositoryTests/UserProviderTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import Repository
+
+final class UserProviderTests: XCTest {
+    
+    func testNothing() throws {
+        // Add your tests here
+        XCTAssert(true)
+    }
+    
+    static let allTests = [
+        ("testNothing", testNothing)
+    ]
+}
