52898fc99c4866951b10c7a5b425bc8e6146fc5f
Omar Alshammari <o.alshammari@Mac-mini.local>
2020-06-18 06:35:38 +0300

    Add Domain Tests and Remove unused tests

---
+++ Tests/DomainTests/UserTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import Application
+
+final class UserTests: XCTestCase {
+    
+    func testNothing() {
+        XCTAssertTrue(true)
+    }
+
+    static let allTests = [
+        ("testNothing", testNothing)
+    ]
+}
